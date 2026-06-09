8dd8c4565d25ab0d28d4352a36e93cf220728c95
deba.ai <dp0000000004@gamil.com>
2026-06-09 16:17:26 +0530

    Updated virsion

---
+++ README.md
@@ -0,0 +1,42 @@
+# 🖇️This is a Leadtracker
+
+- Are u forgot leads, No worries it take care of you❤️.
+
+## 🚀Features
+
+- It takes the input(urls) from the user and save in localStorage, and every time user came back to extension it stay here till u delete it manualy
+
+- It also save the tab automatic in one click so no worries. it is fast.
+
+## 📘User Manual
+
+- One Click on Save Input To save The Input Box item.
+- One Click on SAve Tab to save The Tab link directly.
+- Double Click in Delete all to  Delete all the Leads.
+
+## Pull requests
+- all are welcome
+
+## Installation
+
+```bash
+git clone git@github.com:dp0000000004-eng/Chrome-Extens01.git
+```
+- Then
+
+```bash
+cd repo 
+```
+- Then Go to 
+```bash
+chrome://extension
+```
+- On developer option
+
+- Then see top left corner for Load unpacked
+
+- then Chose the the repo directory
+
+# Licence
+- Made by Debasish pradhan
+
--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 
-let myLeads = []
+
+
+let myLeads = JSON.parse(localStorage.getItem("myLeads")) || []
 const inputEl = document.getElementById("input-El")
 const btnEl = document.getElementById("input-Btn")
-let ulEl = document.getElementById("link-El")
+const ulEl = document.getElementById("link-El")
+const deleteBtn = document.getElementById("clear-Btn")
+const tab = document.getElementById("saveTab-Btn")
 
-localStorage.setItem("myLeads", myLeads[i])
+if (myLeads) {
+    render(myLeads)
+}
 
-btnEl.addEventListener("click", function() {
+
+btnEl.addEventListener("click", function(event) {
+    event.preventDefault()
     const inputVal = inputEl.value;
     myLeads.push(inputVal)
-    for(let i = 0; i < myLeads.length; i++) {
-        localStorage.setItem("myLeads", myLeads[i])
-    }
-    ulEl.innerHTML = `<li> 
-                            <a href="${ localStorage.getItem("myLeads") }" target="_blank">
-                                ${localStorage.getItem("myLeads")} 
-                            </a> 
-                     </li>`
-    inputEl.value = '';
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    inputEl.value = ""; 
 })
 
 
+//*reuseable
+function render(leads) {
+    let listItem = ""
+    for(let i = 0; i < leads.length; i++) {
+        listItem += `<li> 
+                        <a href="${leads[i]}" target="_blank">
+                            ${leads[i]} 
+                        </a> 
+                    </li>`
+    }
+    ulEl.innerHTML = listItem;
+}
+
+
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    ulEl.innerHTML = "";
+})
+
+tab.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
